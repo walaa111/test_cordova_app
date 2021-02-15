2c5bbf31716b6b64d931daa3667f48e286c5b0f0
walaa111 <walaagamil18@gmail.com>
2021-02-15 22:25:04 +0200

    edit

---
+++ test_cordova/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,26 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-connectivity-monitor.connectivity",
+      "file": "plugins/cordova-connectivity-monitor/www/connectivity.js",
+      "pluginId": "cordova-connectivity-monitor",
+      "clobbers": [
+        "window.connectivity"
+      ]
+    },
+    {
+      "id": "cordova-admob.AdMobAds",
+      "file": "plugins/cordova-admob/www/admob.js",
+      "pluginId": "cordova-admob",
+      "clobbers": [
+        "window.admob",
+        "window.tappx"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-connectivity-monitor": "1.2.2",
+    "cordova-play-services-version-adapter": "1.0.2",
+    "cordova-admob": "5.1.0"
+  };
+});
