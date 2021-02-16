0714001593b035deae81181be2fcabc8d807fdfb
walaa111 <walaagamil18@gmail.com>
2021-02-16 22:52:12 +0200

    ed

--- new_cordova/www/js/index.js
+++ new_cordova/www/js/index.js
@@ -1,66 +1,23 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-  // Application Constructor
-  initialize: function() {
-      document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-  },
-
-  // deviceready Event Handler
-  //
-  // Bind any cordova events here. Common events are:
-  // 'pause', 'resume', etc.
-  onDeviceReady: function() {
-      this.receivedEvent('deviceready');
-      // Add to index.js or the first page that loads with your app.
+// Add to index.js or the first page that loads with your app.
 // For Intel XDK and please add this to your app.js.
 
 document.addEventListener('deviceready', function () {
-// Enable to debug issues.
-// window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  alert("yes");
+  //Remove this method to stop OneSignal Debugging 
+ // window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 0});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    
+   alert('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
 
-var notificationOpenedCallback = function(jsonData) {
-  alert('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-};
+  window.plugins.OneSignal
+    .startInit("8ec2c368-d57d-4f2f-be1f-4686aa16c3e0")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .iOSSettings(iosSettings)
+    .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
+    .endInit();
+  
 
-window.plugins.OneSignal
-  .startInit("8ec2c368-d57d-4f2f-be1f-4686aa16c3e0")
-  .handleNotificationOpened(notificationOpenedCallback)
-  .endInit();
-
-// Call syncHashedEmail anywhere in your app if you have the user's email.
-// This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
-// window.plugins.OneSignal.syncHashedEmail(userEmail);
+  
 }, false);
-  },
-
-  // Update DOM on a Received Event
-  receivedEvent: function(id) {
-      var parentElement = document.getElementById(id);
-      var listeningElement = parentElement.querySelector('.listening');
-      var receivedElement = parentElement.querySelector('.received');
-
-      listeningElement.setAttribute('style', 'display:none;');
-      receivedElement.setAttribute('style', 'display:block;');
-
-      console.log('Received Event: ' + id);
-  }
-};
-
-app.initialize();
