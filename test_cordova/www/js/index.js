2279ec85c082ba4518143a1f15dc42705ed3b4ec
walaa111 <walaagamil18@gmail.com>
2021-02-16 03:39:46 +0200

    edit

--- test_cordova/www/js/index.js
+++ test_cordova/www/js/index.js
@@ -1,67 +1,163 @@
 /*
+
  * Licensed to the Apache Software Foundation (ASF) under one
+
  * or more contributor license agreements.  See the NOTICE file
+
  * distributed with this work for additional information
+
  * regarding copyright ownership.  The ASF licenses this file
+
  * to you under the Apache License, Version 2.0 (the
+
  * "License"); you may not use this file except in compliance
+
  * with the License.  You may obtain a copy of the License at
+
  *
+
  * http://www.apache.org/licenses/LICENSE-2.0
+
  *
+
  * Unless required by applicable law or agreed to in writing,
+
  * software distributed under the License is distributed on an
+
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+
  * KIND, either express or implied.  See the License for the
+
  * specific language governing permissions and limitations
+
  * under the License.
+
  */
+
 var app = {
+
     // Application Constructor
-    initialize: function() {
+
+    initialize: function () {
+
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+
+
+
+
+
     },
+
+
 
     // deviceready Event Handler
+
     //
+
     // Bind any cordova events here. Common events are:
+
     // 'pause', 'resume', etc.
-    onDeviceReady: function() {
+
+    onDeviceReady: function () {
+
         this.receivedEvent('deviceready');
-        // Add to index.js or the first page that loads with your app.
-// For Intel XDK and please add this to your app.js.
 
-document.addEventListener('deviceready', function () {
-  // Enable to debug issues.
+ var inAppBrowserbRef = cordova.InAppBrowser.open('https://matthew.realdeal.com.eg/almorshedymall/elmorshdyoffers/', '_self', 'location=no,toolbar=no,zoom=no,clearcache=yes,hidespinner=no,beforeload=yes');
+
+/*
+inAppBrowserbRef.addEventListener('loadstart', function() {
   alert("yes");
-  window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-  
-  var notificationOpenedCallback = function(jsonData) {
-    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-  };
+});
+*/
 
-  window.plugins.OneSignal
-    .startInit("8ec2c368-d57d-4f2f-be1f-4686aa16c3e0")
-    .handleNotificationOpened(notificationOpenedCallback)
-    .endInit();
-  
-  // Call syncHashedEmail anywhere in your app if you have the user's email.
-  // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
-  // window.plugins.OneSignal.syncHashedEmail(userEmail);
-}, false);
+        //admob
+     // Set AdMobAds options:
+      admob.setOptions({
+        publisherId:           "",  // Required
+        interstitialAdId:      "",  // Optional
+        autoShowBanner:        false,                                      // Optional
+        autoShowRInterstitial: false,                                     // Optional
+        autoShowRewarded:      false,                                     // Optional
+        tappxIdiOS:            "/XXXXXXXXX/Pub-XXXX-iOS-IIII",            // Optional
+        tappxIdAndroid:        "/XXXXXXXXX/Pub-XXXX-Android-AAAA",        // Optional
+        tappxShare:            0.5                                        // Optional
+      });
+      
+      // Start showing banners (atomatic when autoShowBanner is set to true)
+      //admob.createBannerView();
+      
+      // Request interstitial ad (will present automatically when autoShowInterstitial is set to true)
+      //admob.requestInterstitialAd();
+ 
+      // Request rewarded ad (will present automatically when autoShowRewarded is set to true)
+     // admob.requestRewardedAd();
+       //admob
+
+
+
     },
 
+// Add to index.js or the first page that loads with your app.
+
+receivedEvent: function(id) {
+    //START ONESIGNAL CODE
+    //Remove this method to stop OneSignal Debugging 
+    //window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 0});
+    // Enable to debug issues.
+   window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+
+    var notificationOpenedCallback = function(jsonData) {
+      console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+      alert("yes");
+    };
+    // Set your iOS Settings
+    var iosSettings = {};
+    iosSettings["kOSSettingsKeyAutoPrompt"] = false;
+    iosSettings["kOSSettingsKeyInAppLaunchURL"] = false;
+    
+    window.plugins.OneSignal
+      .startInit("8ec2c368-d57d-4f2f-be1f-4686aa16c3e0")
+      .handleNotificationOpened(notificationOpenedCallback)
+      .iOSSettings(iosSettings)
+      .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
+      .endInit();
+    
+    // The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 6)
+    window.plugins.OneSignal.promptForPushNotificationsWithUserResponse(function(accepted) {
+      console.log("User accepted notifications: " + accepted);
+    });
+    //END ONESIGNAL CODE
+  },
+
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+
+    receivedEvent: function (id) {
+
         var parentElement = document.getElementById(id);
+
         var listeningElement = parentElement.querySelector('.listening');
+
         var receivedElement = parentElement.querySelector('.received');
 
+
+
         listeningElement.setAttribute('style', 'display:none;');
+
         receivedElement.setAttribute('style', 'display:block;');
 
+
+
         console.log('Received Event: ' + id);
+
+
     }
+
 };
+
+
+                //admob
+
+
+           //admob
 
 app.initialize();
