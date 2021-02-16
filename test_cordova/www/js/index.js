300c56cb612ac906fa9cbae447b49f17a9ae860b
walaa111 <walaagamil18@gmail.com>
2021-02-16 03:22:22 +0200

    edut

--- test_cordova/www/js/index.js
+++ test_cordova/www/js/index.js
@@ -33,7 +33,8 @@ var app = {
 
 document.addEventListener('deviceready', function () {
   // Enable to debug issues.
-  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  alert("yes");
+  window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
   
   var notificationOpenedCallback = function(jsonData) {
     console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
