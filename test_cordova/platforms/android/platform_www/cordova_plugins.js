be8cb39a3b7538598344ad85a50cd8691c2edb20
walaa111 <walaagamil18@gmail.com>
2021-02-16 18:08:26 +0200

    ed

--- test_cordova/platforms/android/platform_www/cordova_plugins.js
+++ test_cordova/platforms/android/platform_www/cordova_plugins.js
@@ -18,14 +18,6 @@ module.exports = [
     ]
   },
   {
-    "id": "cordova-plugin-inappbrowser.inappbrowser",
-    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
-    "pluginId": "cordova-plugin-inappbrowser",
-    "clobbers": [
-      "cordova.InAppBrowser.open"
-    ]
-  },
-  {
     "id": "cordova-plugin-firebase.FirebasePlugin",
     "file": "plugins/cordova-plugin-firebase/www/firebase.js",
     "pluginId": "cordova-plugin-firebase",
@@ -34,28 +26,27 @@ module.exports = [
     ]
   },
   {
-    "id": "phonegap-plugin-push.PushNotification",
-    "file": "plugins/phonegap-plugin-push/www/push.js",
-    "pluginId": "phonegap-plugin-push",
+    "id": "cordova-plugin-inappbrowser.inappbrowser",
+    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+    "pluginId": "cordova-plugin-inappbrowser",
     "clobbers": [
-      "PushNotification"
+      "cordova.InAppBrowser.open"
     ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.4",
   "cordova-connectivity-monitor": "1.2.2",
   "cordova-play-services-version-adapter": "1.0.2",
   "cordova-admob": "5.1.0",
-  "cordova-plugin-inappbrowser": "4.1.0",
-  "cordova-plugin-androidx": "3.0.0",
-  "cordova-plugin-firebase": "2.0.5",
   "cordova-android-play-services-gradle-release": "4.0.0",
+  "cordova-plugin-androidx": "3.0.0",
+  "cordova-plugin-firebase": "2.0.0",
+  "cordova-plugin-inappbrowser": "4.1.0",
+  "cordova-plugin-whitelist": "1.3.4",
   "cordova-support-google-services": "1.3.2",
-  "phonegap-plugin-multidex": "1.0.0",
-  "phonegap-plugin-push": "2.3.0"
+  "phonegap-plugin-multidex": "1.0.0"
 };
 // BOTTOM OF METADATA
 });
