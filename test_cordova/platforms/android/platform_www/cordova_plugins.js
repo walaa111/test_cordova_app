56d3342a101ffebfa5640f777562ab54b58f6a90
walaa111 <walaagamil18@gmail.com>
2021-02-16 03:26:30 +0200

    edut

--- test_cordova/platforms/android/platform_www/cordova_plugins.js
+++ test_cordova/platforms/android/platform_www/cordova_plugins.js
@@ -53,7 +53,7 @@ module.exports.metadata =
   "cordova-plugin-androidx": "3.0.0",
   "cordova-plugin-firebase": "2.0.5",
   "cordova-android-play-services-gradle-release": "4.0.0",
-  "onesignal-cordova-plugin": "1.13.1"
+  "onesignal-cordova-plugin": "2.11.3"
 };
 // BOTTOM OF METADATA
 });
