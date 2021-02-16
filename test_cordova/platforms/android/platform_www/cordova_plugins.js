d4253636cc1730c30aa1a374b5239e866243f72b
walaa111 <walaagamil18@gmail.com>
2021-02-16 16:07:02 +0200

    edit

--- test_cordova/platforms/android/platform_www/cordova_plugins.js
+++ test_cordova/platforms/android/platform_www/cordova_plugins.js
@@ -34,11 +34,11 @@ module.exports = [
     ]
   },
   {
-    "id": "onesignal-cordova-plugin.OneSignal",
-    "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
-    "pluginId": "onesignal-cordova-plugin",
+    "id": "phonegap-plugin-push.PushNotification",
+    "file": "plugins/phonegap-plugin-push/www/push.js",
+    "pluginId": "phonegap-plugin-push",
     "clobbers": [
-      "OneSignal"
+      "PushNotification"
     ]
   }
 ];
@@ -53,7 +53,9 @@ module.exports.metadata =
   "cordova-plugin-androidx": "3.0.0",
   "cordova-plugin-firebase": "2.0.5",
   "cordova-android-play-services-gradle-release": "4.0.0",
-  "onesignal-cordova-plugin": "2.3.3"
+  "cordova-support-google-services": "1.3.2",
+  "phonegap-plugin-multidex": "1.0.0",
+  "phonegap-plugin-push": "2.3.0"
 };
 // BOTTOM OF METADATA
 });
