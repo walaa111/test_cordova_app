650cefcdf4df22a29170fc124af39188a1f368a3
walaa111 <walaagamil18@gmail.com>
2021-02-17 21:50:33 +0200

    ed

--- newappcordova/plugins/cordova-plugin-fcm/scripts/fcm_config_files_process.js
+++ newappcordova/plugins/cordova-plugin-fcm/scripts/fcm_config_files_process.js
@@ -55,10 +55,10 @@ if (directoryExists("platforms/android")) {
     if (fileExists( path )) {
       try {
         var contents = fs.readFileSync(path).toString();
-        fs.writeFileSync("platforms/android/google-services.json", contents);
+        fs.writeFileSync("platforms/android/app/google-services.json", contents);
 
         var json = JSON.parse(contents);
-        var strings = fs.readFileSync("platforms/android/res/values/strings.xml").toString();
+        var strings = fs.readFileSync("./platforms/android/app/src/main/res/values/strings.xml").toString();
 
         // strip non-default value
         strings = strings.replace(new RegExp('<string name="google_app_id">([^\@<]+?)</string>', "i"), '')
@@ -75,7 +75,7 @@ if (directoryExists("platforms/android")) {
         // replace the default value
         strings = strings.replace(new RegExp('<string name="google_api_key">([^<]+?)</string>', "i"), '<string name="google_api_key">' + json.client[0].api_key[0].current_key + '</string>')
 
-        fs.writeFileSync("platforms/android/res/values/strings.xml", strings);
+        fs.writeFileSync("./platforms/android/app/src/main/res/values/strings.xml", strings);
       } catch(err) {
         process.stdout.write(err);
       }
