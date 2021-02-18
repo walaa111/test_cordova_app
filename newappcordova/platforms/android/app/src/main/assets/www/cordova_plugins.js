b115621b38b45a7a82f02e16622216a0ff523ca4
walaa111 <walaagamil18@gmail.com>
2021-02-18 04:30:07 +0200

    ed

--- newappcordova/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ newappcordova/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -13,7 +13,9 @@ module.exports.metadata =
 // TOP OF METADATA
 {
   "cordova-plugin-fcm": "2.1.2",
-  "cordova-plugin-whitelist": "1.3.4"
+  "cordova-plugin-whitelist": "1.3.4",
+  "cordova-support-google-services": "1.4.1",
+  "cordova-plugin-fcm-config": "1.0.0"
 };
 // BOTTOM OF METADATA
 });
--- newappcordova/platforms/android/platform_www/cordova_plugins.js
+++ newappcordova/platforms/android/platform_www/cordova_plugins.js
@@ -14,7 +14,8 @@ module.exports.metadata =
 {
   "cordova-plugin-fcm": "2.1.2",
   "cordova-plugin-whitelist": "1.3.4",
-  "cordova-support-google-services": "1.4.1"
+  "cordova-support-google-services": "1.4.1",
+  "cordova-plugin-fcm-config": "1.0.0"
 };
 // BOTTOM OF METADATA
 });
