04d8bde91fcebe4ca15ac80e7952f14993872345
walaa111 <walaagamil18@gmail.com>
2021-02-18 19:21:55 +0200

    ed

--- newappcordova/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ newappcordova/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -7,13 +7,22 @@ module.exports = [
     "clobbers": [
       "FCMPlugin"
     ]
+  },
+  {
+    "id": "onesignal-cordova-plugin.OneSignal",
+    "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
+    "pluginId": "onesignal-cordova-plugin",
+    "clobbers": [
+      "OneSignal"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
   "cordova-plugin-fcm": "2.1.2",
-  "cordova-plugin-whitelist": "1.3.4"
+  "cordova-plugin-whitelist": "1.3.4",
+  "onesignal-cordova-plugin": "2.11.3"
 };
 // BOTTOM OF METADATA
 });
