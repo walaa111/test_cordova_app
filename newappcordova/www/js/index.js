f24497fe9dd2b36c5628bfa4ad56e6e32488e7da
walaa111 <walaagamil18@gmail.com>
2021-02-18 19:31:21 +0200

    ed

--- newappcordova/www/js/index.js
+++ newappcordova/www/js/index.js
@@ -12,6 +12,7 @@ receivedEvent: function(id) {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+        /*
 		FCMPlugin.onTokenRefresh(function(token){
             alert( token );
 			var div = document.getElementById('divToken');
@@ -34,7 +35,31 @@ receivedEvent: function(id) {
               console.log('Error registering onNotification callback: ' + err);
             }
           );
+*/
 
+  //Remove this method to stop OneSignal Debugging 
+  window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 0});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+  // Set your iOS Settings
+  var iosSettings = {};
+  iosSettings["kOSSettingsKeyAutoPrompt"] = false;
+  iosSettings["kOSSettingsKeyInAppLaunchURL"] = false;
+  
+  window.plugins.OneSignal
+    .startInit("8ec2c368-d57d-4f2f-be1f-4686aa16c3e0")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .iOSSettings(iosSettings)
+    .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
+    .endInit();
+  
+  // The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 6)
+  window.plugins.OneSignal.promptForPushNotificationsWithUserResponse(function(accepted) {
+    console.log("User accepted notifications: " + accepted);
+  });
+  //END ONESIGNAL CODE
         console.log('Received Event: ' + id);
     }
 };
