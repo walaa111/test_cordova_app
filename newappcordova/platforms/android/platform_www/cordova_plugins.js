ec9d565679162853d495502bdaf1b23fe238813c
walaa111 <walaagamil18@gmail.com>
2021-02-18 18:27:06 +0200

    ed

--- newappcordova/platforms/android/platform_www/cordova_plugins.js
+++ newappcordova/platforms/android/platform_www/cordova_plugins.js
@@ -1,21 +1,20 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
-    "id": "cordova-plugin-fcm.FCMPlugin",
-    "file": "plugins/cordova-plugin-fcm/www/FCMPlugin.js",
-    "pluginId": "cordova-plugin-fcm",
+    "id": "cordova-plugin-fcm-with-dependecy-updated.FCMPlugin",
+    "file": "plugins/cordova-plugin-fcm-with-dependecy-updated/www/FCMPlugin.js",
+    "pluginId": "cordova-plugin-fcm-with-dependecy-updated",
     "clobbers": [
-      "FCMPlugin"
+      "FCM"
     ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-fcm": "2.1.2",
   "cordova-plugin-whitelist": "1.3.4",
   "cordova-support-google-services": "1.4.1",
-  "cordova-plugin-fcm-config": "1.0.0"
+  "cordova-plugin-fcm-with-dependecy-updated": "7.8.0"
 };
 // BOTTOM OF METADATA
 });
